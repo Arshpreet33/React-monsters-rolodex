3e31c6c8b8af944ef9c1aa2079d5d3f60a4ed327
Arshpreet33 <arshpreetsingh33@gmail.com>
2023-02-01 22:20:35 -0700

    Initial commit - complete project in class architecture.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // monsters: [{ name: 'Linda' }, { name: 'Frank' }, { name: 'Jackie' }],
+      monsters: [],
+      searchName: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  onSearchTextChange = (event) => {
+    const searchName = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchName };
+    });
+  };
+
+  render() {
+    const { monsters, searchName } = this.state;
+    const { onSearchTextChange } = this;
+    var filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLocaleLowerCase().includes(searchName)
+    );
+    return (
+      <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
+
+        <SearchBox
+          className="monsters-search-box"
+          placeholder="search monsters"
+          onChangeHandler={onSearchTextChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>Arshpreet Singh</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
